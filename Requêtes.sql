811bbe54249f527822b253bf49d0aa4ae2ee3b53
Zicka67 <art.book@hotmail.fr>
2023-01-26 16:57:25 +0100

    Ajout exercice 7 8

--- Requêtes.sql
+++ Requêtes.sql
@@ -43,27 +43,38 @@
 -- ORDER BY SUM(qte*cout_ingredient) DESC
 
 -- 7. Nom des ingrédients + coût + quantité de chaque ingrédient qui composent la potion 'Santé'.
-SELECT nom_ingredient, cout_ingredient, qtt
 
-
+--  SELECT nom_ingredient, cout_ingredient,qte
+--  FROM ingredient
+--  INNER JOIN composer ON ingredient.id_ingredient= composer.id_ingredient
+--  INNER JOIN potion ON composer.id_potion= potion.id_potion
+--  WHERE lower(nom_potion) LIKE 'santé'
+ 
 -- 8. Nom du ou des personnages qui ont pris le plus de casques dans la bataille 'Bataille du village 
 -- gaulois'.
 
+-- SELECT nom_personnage, qte
+-- FROM personnage
+-- INNER JOIN prendre_casque ON personnage.id_personnage= prendre_casque.id_personnage
+-- INNER JOIN bataille ON prendre_casque.id_bataille= bataille.id_bataille
+-- WHERE (nom_bataille) LIKE 'Bataille du village gaulois'
+-- ORDER BY (qte) DESC
+-- LIMIT 1
+
 -- 9. Nom des personnages et leur quantité de potion bue (en les classant du plus grand buveur 
 -- au plus petit).
-
 -- 10. Nom de la bataille où le nombre de casques pris a été le plus important.
-
 -- 11. Combien existe-t-il de casques de chaque type et quel est leur coût total ? (classés par 
 -- nombre décroissant)
-
 -- 12. Nom des potions dont un des ingrédients est le poisson frais.
-
 -- 13. Nom du / des lieu(x) possédant le plus d'habitants, en dehors du village gaulois.
-
 -- 14. Nom des personnages qui n'ont jamais bu aucune potion.
-
 -- 15. Nom du / des personnages qui n'ont pas le droit de boire de la potion 'Magique'.
+
+
+
+ 
+
 
 
 
