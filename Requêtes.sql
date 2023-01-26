10a12f8a36547814432f604dd6bf200b1b5c640a
Zicka67 <art.book@hotmail.fr>
2023-01-26 14:46:24 +0100

    Ajout des exercices 2 et 3

--- Requêtes.sql
+++ Requêtes.sql
@@ -1,5 +1,19 @@
+-- 1. Nom des lieux qui finissent par 'um'.
+
 -- SELECT *
 -- FROM lieu
 -- WHERE nom_lieu LIKE '%um'
 -- 
 
+-- 2. Nombre de personnages par lieu (trié par nombre de personnages décroissant).
+
+-- SELECT COUNT(id_personnage) ,id_lieu
+-- FROM personnage
+-- GROUP BY id_lieu
+
+-- 3. Nom des personnages + spécialité + adresse et lieu d'habitation, triés par lieu puis par nom 
+-- de personnage
+
+-- SELECT nom_personnage, id_specialite, adresse_personnage, id_lieu
+-- FROM personnage
+-- GROUP BY id_lieu, nom_personnage
